88386a80c52f1be9b963a0abeab7dd54517c7c68
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-06-30 15:53:43 -0400

    Initial commit

---
+++ WitchHuntStandard/ParsedPattern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WitchHuntStandard
+{
+    public ref struct ParsedPattern
+    {
+        public Span<byte> BytesToSearch;
+
+        public Span<byte> Mask;
+
+        public Span<string> PostPattern;
+    }
+}
